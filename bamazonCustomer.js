ce3f63357ad577a918f41731bcb3d077ef41985e
Gregg Hosie <gregg.hosie@gmail.com>
2019-01-27 20:54:01 -0500

    price calculations and quantity check against db

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,5 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -10,21 +11,45 @@ var connection = mysql.createConnection({
   user: "root",
 
   // Your password
-  password: "",
-  database: "bamazon_DB"
+  password: "08816674b",
+  database: "bamazon_db"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
-  queryAllproducts();
+  queryAllProducts();
+  
 });
 
+
 function queryAllProducts() {
-  connection.query("SELECT * FROM products", function(err, res) {
+  connection.query("SELECT * FROM products", function (err, res) {
     for (var i = 0; i < res.length; i++) {
       console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price);
     }
     console.log("-----------------------------------");
+    newPurchase();
   });
 }
+
+function Order(item_id, quantity) {
+  this.item_id = item_id;
+  this.quanity = quantity;
+}
+
+function newPurchase() {
+  inquirer.prompt([{
+    name: "item_id",
+    message: "What is the item id of the product you would like to order?"
+  }, {
+    name: "quantity",
+    message: "How many would you like to purchase?"
+  }]).then(function (answers) {
+    // initializes the variable newProgrammer to be a programmer object which will take
+    // in all of the user's answers to the questions above
+    var newOrder = new Order(answers.item_id, answers.quantity);
+    // printInfo method is run to show that the newProgrammer object was successfully created and filled
+    console.log(newOrder);
+  });
+};
